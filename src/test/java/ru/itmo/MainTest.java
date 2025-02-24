00c4a6c37273729d4dd96ba42e41865e6f7b8eea
Dmitrii Karagodin <4319788@gmail.com>
2025-02-24 09:31:11 +0300

    Add template project

---
+++ src/test/java/ru/itmo/MainTest.java
@@ -0,0 +1,12 @@
+package ru.itmo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
