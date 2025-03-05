41630686902b64fc792997a2456b1f353dca923e
Bubelov Sergey <sergei.bubelov1@gmail.com>
2025-03-05 19:20:28 +0300

    feat: calculator

--- src/main/java/ru/itmo/Calculator.java
+++ src/main/java/ru/itmo/Calculator.java
@@ -1,11 +1,11 @@
 package ru.itmo;
 
 public interface Calculator {
-	int sum(int x, int y);
+    int sum(int x, int y);
 
-	int sub(int x, int y);
+    int sub(int x, int y);
 
-	int mul(int x, int y);
+    int mul(int x, int y);
 
-	double div(int x, int y);
+    double div(int x, int y);
 }
