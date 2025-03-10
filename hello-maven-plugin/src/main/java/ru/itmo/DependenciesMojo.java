188a603d9ab7b80dde345e9326e33534c7d03434
Bubelov Sergey <sergei.bubelov1@gmail.com>
2025-03-10 22:42:26 +0300

    feat: all tasks

--- hello-maven-plugin/src/main/java/ru/itmo/DependenciesMojo.java
+++ hello-maven-plugin/src/main/java/ru/itmo/DependenciesMojo.java
@@ -1,5 +1,29 @@
 package ru.itmo;
 
-public class DependenciesMojo {
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
+import java.util.List;
+
+@Mojo(name = "deps", defaultPhase = LifecyclePhase.PACKAGE)
+public class DependenciesMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(property = "scope")
+    String scope;
+
+    public void execute() {
+        List<Dependency> dependencies = project.getDependencies();
+        long numDependencies = dependencies.stream()
+                .filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope()))
+                .count();
+        getLog().info("Number of dependencies: " + numDependencies);
+    }
 }
